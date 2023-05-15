a2ddec6a3fe181691f2b21b2a3cbf230832ffe97
marlinfiggins <marlinfiggins@gmail.com>
2023-05-15 16:13:25 -0700

    Adding support for hierarchical MLR models

--- scripts/run-mlr-model.py
+++ scripts/run-mlr-model.py
@@ -124,8 +124,28 @@ class MLRConfig:
         return fit, save, load, export_json, export_path
 
 
-def fit_models(rs, locations, model, inference_method, path, save, pivot=None):
+
+
+def fit_models(rs, locations, model, inference_method, hier, path, save, pivot=None):
     multi_posterior = ef.MultiPosterior()
+
+    if hier:
+        # Subset data to locations of interest
+        raw_seq = rs[rs.location.isin(locations)]
+        data = ef.HierFrequencies(raw_seq=raw_seq, pivot=pivot, group="location")
+        
+        # Fit model
+        posterior = inference_method.fit(model, data, name="hierarchical")
+
+        #TODO: Add forecast method for these hierarchical models
+        n_days_to_present = (pd.to_datetime(date.today()) - data.dates[-1]).days
+        n_days_to_forecast = n_days_to_present + model.forecast_L
+        # model.forecast_frequencies(posterior.samples, forecast_L=n_days_to_forecast)
+
+        multi_posterior.add_posterior(posterior=posterior)
+
+        if save:
+            posterior.save_posterior(f"{path}/model/hierarchical.json")
 
     for location in locations:
         # Subset to data of interest
@@ -188,13 +208,39 @@ def make_model_directories(path):
     make_path_if_absent(path + "/models")
 
 
-def export_results(multi_posterior, ps, path, data_name):
+def export_results(multi_posterior, ps, path, data_name, hier):
     EXPORT_SITES = ["freq", "ga", "freq_forecast"]
     EXPORT_DATED = [True, False, True]
     EXPORT_FORECASTS = [False, False, True]
     EXPORT_ATTRS = ["pivot"]
     # Make directories
     make_model_directories(path)
+    
+    # Split hierarchical results into group posteriors
+    if hier:
+        def get_group_samples(samples, sites, group):
+            samples_group = dict()
+            for site in sites:
+                samples_group[site] = samples[site][..., group]
+            return samples_group
+
+        mp = multi_posterior
+        hier_samples = mp.locator["hierarchical"].samples
+        multi_posterior = ef.MultiPosterior()
+        for n, name in enumerate(mp.data.groups):
+            multi_posterior.add_posterior(
+                 ef.PosteriorHandler(
+                    samples=get_group_samples(hier_samples, EXPORT_SITES, n),
+                    data=mp.data.groups[n],
+                    name=name)
+            )
+        # Add final posterior for hierarchical growth advantages
+        multi_posterior.add_posterior(
+            ef.PosteriorHandler(
+                samples={"ga": hier_samples["ga_loc"]},
+                data=mp.data,
+                name="hierarchical")
+        )
 
     # Combine jsons from multiple model runs
     results = []
@@ -271,6 +317,8 @@ if __name__ == "__main__":
     print("pivot", pivot)
 
     # Fit or load model results
+    hier = config.config["model"]["hierarchical"] or False
+
     if fit:
         print("Fitting model")
         multi_posterior = fit_models(
@@ -278,6 +326,7 @@ if __name__ == "__main__":
             locations,
             mlr_model,
             inference_method,
+            hier,
             export_path,
             save,
             pivot=pivot
@@ -301,4 +350,4 @@ if __name__ == "__main__":
             config.config["settings"], "ps", dflt=[0.5, 0.8, 0.95]
         )
         data_name = args.data_name or config.config["data"]["name"]
-        export_results(multi_posterior, ps, export_path, data_name)
+        export_results(multi_posterior, ps, export_path, data_name, hier)
