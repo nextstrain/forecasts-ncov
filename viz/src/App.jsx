1073e78ca60f94406cce694cce2011cf5a81fc99
James Hadfield <hadfield.james@gmail.com>
2023-05-17 10:45:54 +1200

    Add model run date to abstract text

--- viz/src/App.jsx
+++ viz/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <p>
           Each line represents the estimated frequency of a particular clade through time.
           Equivalent Pango lineage is given in parenthesis, eg clade 22B (lineage BA.5).
+          Model run date: {mlrCladesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="cladeFrequenciesPanel" class="panelDisplay"> {/* surrounding div(s) used for static-images.js script */}
           <PanelDisplay data={mlrCladesData} params={{preset: "frequency"}}/>
@@ -25,6 +26,7 @@ function App() {
           These plots show the estimated growth advantage for given clades relative to clade
           22B (lineage BA.5). This describes how many more secondary infections a variant causes
           on average relative to clade 22B. Vertical bars show the 95% HPD.
+          Model run date: {mlrCladesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="cladeGrowthAdvantagePanel" class="panelDisplay">
           <PanelDisplay data={mlrCladesData} params={{preset: "growthAdvantage"}}/>
@@ -34,6 +36,7 @@ function App() {
         <p>
           Each line represents the estimated frequency of a particular Pango lineage through time.
           Lineages with fewer than 200 observations are collapsed into parental lineage.
+          Model run date: {mlrLineagesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="lineageFrequenciesPanel" class="panelDisplay">
           <PanelDisplay data={mlrLineagesData} params={{preset: "frequency"}}/>
@@ -44,6 +47,7 @@ function App() {
           These plots show the estimated growth advantage for given Pango lineages relative to
           lineage BA.5. This describes how many more secondary infections a variant causes
           on average relative to lineage BA.5. Vertical bars show the 95% HPD.
+          Model run date: {mlrLineagesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="lineageGrowthAdvantagePanel" class="panelDisplay">
           <PanelDisplay data={mlrLineagesData} params={{preset: "growthAdvantage"}}/>
