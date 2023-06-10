5d6a4f356d9b0c25646ef1e1c6ade06cd9c60ea8
Trevor Bedford <trevor@bedford.io>
2023-06-09 18:56:22 -0700

    Update pivot to 23A (XBB.1.5)
    BA.5 was dropping from the Pango lineages analysis due to collapse lineage counts (leaving BA.5.1 and BA.5.2). This was causing problems with the pivot being absent. And only a short matter of time before it disappears from clades as well.
    
    In updating to a new pivot we need a clade and a corresponding lineage that appears across geographies. XBB (which might be more natural) didn't work for this reason (basically absent from Canada).
    
    I chose clade 23A (lineage XBB.1.5) as it's well represented across countries and should give us a fair amount of time before it needs updating again.

--- viz/src/App.jsx
+++ viz/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <h2>Clade frequencies over time</h2>
         <p>
           Each line represents the estimated frequency of a particular clade through time.
-          Equivalent Pango lineage is given in parenthesis, eg clade 22B (lineage BA.5).
+          Equivalent Pango lineage is given in parenthesis, eg clade 23A (lineage XBB.1.5).
           Results last updated {mlrCladesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="cladeFrequenciesPanel" class="panelDisplay"> {/* surrounding div(s) used for static-images.js script */}
@@ -24,8 +24,8 @@ function App() {
         <h2>Clade growth advantage</h2>
         <p>
           These plots show the estimated growth advantage for given clades relative to clade
-          22B (lineage BA.5). This describes how many more secondary infections a variant causes
-          on average relative to clade 22B. Vertical bars show the 95% HPD.
+          23A (lineage XBB.1.5). This describes how many more secondary infections a variant causes
+          on average relative to clade 23A. Vertical bars show the 95% HPD.
           Results last updated {mlrCladesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="cladeGrowthAdvantagePanel" class="panelDisplay">
@@ -45,8 +45,8 @@ function App() {
         <h2>Lineage growth advantage</h2>
         <p>
           These plots show the estimated growth advantage for given Pango lineages relative to
-          lineage BA.5. This describes how many more secondary infections a variant causes
-          on average relative to lineage BA.5. Vertical bars show the 95% HPD.
+          lineage XBB.1.5. This describes how many more secondary infections a variant causes
+          on average relative to lineage XBB.1.5. Vertical bars show the 95% HPD.
           Results last updated {mlrLineagesData?.modelData?.get('updated') || 'loading'}.
         </p>
         <div id="lineageGrowthAdvantagePanel" class="panelDisplay">
